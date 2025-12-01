b10f23d2ef16234fed174ba2949a4ae4052fb179
Lucas Brossier <lucasbrossier95@gmail.com>
2025-12-01 09:50:58 +0100

    Bump version to 1.3.0

--- todo/settings.py
+++ todo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=8e&h2naz6*na6mne4y8l1m@rr=(igde^7rz2cmal)r_o)raoo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-VERSION = '1.2.0'
+VERSION = '1.3.0'
 
 ALLOWED_HOSTS = []
 
